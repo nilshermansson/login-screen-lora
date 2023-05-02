d185121d4b3bea78394628ba83b76869f5be7e4d
Nils Hermansson <hermansson.nils@gmail.com>
2023-05-02 20:59:13 +0200

    Added readme, fixed sample.py

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # login-screen-lora
+
+A github repo containing the LoRA weights for a fine-tuned model which was trained on Stable Diffusion 1.5.
+
+## Dependencies
+
+- Python
+- pip
+- a Nvidia graphics card with cuda enabled
+
+## Setup
+
+- Install dependencies with pip by executing the command: ```sh pip install -r requirements.txt```
+
+## Inference
+
+In order to perform inference all you need to do is to run the sample.py file. The model works best if you provide it with a prompt like this:
--- sample.py
+++ sample.py
@@ -3,12 +3,24 @@ import os
 from diffusers import StableDiffusionPipeline
 
 num_inferences = 4
-prompt = input("Enter a prompt: ")
+
+while True:
+    style = input("Select an option (vivid or minimalistic): ").lower()
+    if style == "vivid":
+        break
+    elif style == "minimalistic":
+        break
+    else:
+        print("Invalid option selected. Please choose either 'vivid' or 'minimalistic'.")
+
+color = input("Select a color: ")
+background = input("Select the contents of the background: ")
+prompt = f"{style} with {color} colors and {background} in the background"
 
 pipe = StableDiffusionPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
 pipe.unet.load_attn_procs("./login_simple")
-# pipe = pipe.to("cuda")
+pipe = pipe.to("cuda")
 
 os.makedirs("samples", exist_ok=True)
 
